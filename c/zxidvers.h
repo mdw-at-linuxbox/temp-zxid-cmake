6a9e03b4abce57f536acb4c9d0bae785c5d88cdf
Marcus Watts <mwatts@redhat.com>
2016-11-18 11:21:20 -0500

    Cleaned up cmakified libzxid.

---
+++ c/zxidvers.h
@@ -0,0 +1,7 @@
+#ifndef _zxidvers_h
+#define _zxidvers_h
+#define ZXID_VERSION 0x000143
+#define ZXID_REL "1.43"
+#define ZXID_COMPILE_DATE "1473786686"
+#define ZXID_REV "Dummy version because zxrev not in tarball or git!"
+#endif
